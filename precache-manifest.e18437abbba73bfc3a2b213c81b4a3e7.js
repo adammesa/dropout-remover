044abc9a2201fcdce1f5fcc63d768f6efc55e97c
Adam <hello@allotrope.io>
2020-03-06 10:10:11 -0800

    Updates

---
+++ precache-manifest.e18437abbba73bfc3a2b213c81b4a3e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedef1c0287a7cfbf3949563e81bd79d",
+    "url": "/dropout-remover/index.html"
+  },
+  {
+    "revision": "b37d414093ec14a8d941",
+    "url": "/dropout-remover/static/css/main.88f54abe.chunk.css"
+  },
+  {
+    "revision": "81f0126a6daba89b7b05",
+    "url": "/dropout-remover/static/js/2.f64f68d3.chunk.js"
+  },
+  {
+    "revision": "25d28d1e17abaa66c691040ed06230fb",
+    "url": "/dropout-remover/static/js/2.f64f68d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37d414093ec14a8d941",
+    "url": "/dropout-remover/static/js/main.5cb4a8d8.chunk.js"
+  },
+  {
+    "revision": "5e2a2df1811681928930",
+    "url": "/dropout-remover/static/js/runtime-main.30823206.js"
+  }
+]);
