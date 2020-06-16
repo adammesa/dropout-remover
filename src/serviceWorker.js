79b5c29569e08bc4ac085109366ea324583d5148
Adam <adammesa@yahoo.ca>
2020-06-15 17:06:20 -0700

    Removed debugging console log

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,7 +21,6 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  console.log("Env public URL: " + process.env.PUBLIC_URL + " window location: " + window.location.href);
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
